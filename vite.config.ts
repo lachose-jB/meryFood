b930e592cf5afb310358d3575e71c50138d4d078
lachose-jB <jadjahouisso@gmail.com>
2025-07-05 15:21:48 +0200

    build update

--- vite.config.ts
+++ vite.config.ts
@@ -16,18 +16,29 @@ export default defineConfig({
         manualChunks: {
           vendor: ['vue', 'vue-router', 'pinia'],
           firebase: ['firebase/app', 'firebase/firestore', 'firebase/storage', 'firebase/auth'],
-          ui: ['@headlessui/vue', '@heroicons/vue']
+          ui: ['@headlessui/vue', '@heroicons/vue'],
         }
       }
     },
-    chunkSizeWarningLimit: 1000 // en kB, par exemple
+    chunkSizeWarningLimit: 1000, // en kB
   },
   server: {
     headers: {
       'X-Content-Type-Options': 'nosniff',
       'X-Frame-Options': 'DENY',
       'X-XSS-Protection': '1; mode=block',
-      'Referrer-Policy': 'strict-origin-when-cross-origin'
+      'Referrer-Policy': 'strict-origin-when-cross-origin',
+      'Permissions-Policy': 'geolocation=(), microphone=(), camera=()',
+      'Strict-Transport-Security': 'max-age=63072000; includeSubDomains; preload',
+      'Content-Security-Policy': 
+        "default-src 'self'; " +
+        "connect-src 'self'; " +
+        "img-src 'self' https://www.google.com https://www.google.fr data:; " +
+        "script-src 'self'; " +
+        "style-src 'self' 'unsafe-inline'; " +
+        "font-src 'self'; " +
+        "frame-src https://www.google.com; " +
+        "form-action 'self';"
     }
   }
 })
