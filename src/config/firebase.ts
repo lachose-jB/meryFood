b20bceca04ac1340dee44f01e2ae441e6b3d294e
lachose-jB <jadjahouisso@gmail.com>
2025-08-08 13:35:13 +0200

    Ok4

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -48,16 +48,21 @@ function validateEnv() {
 }
 validateEnv();
 
+import { initializeApp } from "firebase/app";
+
 const firebaseConfig = {
-  apiKey: VITE_FIREBASE_API_KEY,
-  authDomain: VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: VITE_FIREBASE_DATABASE_URL,
-  projectId: VITE_FIREBASE_PROJECT_ID,
-  storageBucket: VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: VITE_FIREBASE_APP_ID,
-  measurementId: VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
+initializeApp(firebaseConfig);
+
 
 // Initialisation de l'app Firebase (unique)
 const app = initializeApp(firebaseConfig);
